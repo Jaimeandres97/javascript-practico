fee9b4cf4831c2d7d948d21cb7c4755237924ade
Jaimeandres97 <ja.moreno12@uniandes.edu.co>
2022-09-19 12:29:13 -0500

    Creacion del componente de menu de carrito de compras y boton de carritos de compras de los productos

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+//Lista de productos
+const productList = [];
+productList.push({
+	name: 'Bike',
+	price: '120',
+	image:
+		'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+	description:
+		'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.',
+});
+
+productList.push({
+	name: 'Pantalla',
+	price: '220',
+	image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg',
+	description: 'A fantastic screen for your work Set-Up',
+});
+
+productList.push({
+	name: 'Balon',
+	price: '60',
+	image:
+		'https://images.pexels.com/photos/47354/the-ball-stadion-football-the-pitch-47354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+	description: 'The new football ball of the world cup',
+});
+
+//Lista carrito compras
+shoppingList = [];
+
 //email que se oprime para desplegar el menu
 const menuEmail = document.querySelector('.navbar-email');
 //menu que se despliega al oprimir el email
@@ -52,11 +81,13 @@ function toggleMobileMenu() {
 }
 //Despliega el menu del carrito de compras
 function toggleCartMenu() {
+	
 	//De esta forma activo la clase inactive en los otros dos menus asi se cierran en el momento de hacer click y el menu deseado se abre
 	mobileMenu.classList.add('inactive');
 	desktopMenu.classList.add('inactive');
 	productDetailContainer.classList.add('inactive');
 	cartIconMenu.classList.toggle('inactive');
+	
 }
 //Abre el banner de informacion de producto al dar click en la imagen, recibe como parametro el producto que se selecciona
 function openProductDetailAside(product) {
@@ -85,32 +116,6 @@ function closeProductDetailAside() {
 	productDetailContainer.classList.toggle('inactive');
 }
 
-//Lista de productos
-const productList = [];
-productList.push({
-	name: 'Bike',
-	price: '120',
-	image:
-		'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-	description:
-		'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.',
-});
-
-productList.push({
-	name: 'Pantalla',
-	price: '220',
-	image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg',
-	description: 'A fantastic screen for your work Set-Up',
-});
-
-productList.push({
-	name: 'Balon',
-	price: '60',
-	image:
-		'https://images.pexels.com/photos/47354/the-ball-stadion-football-the-pitch-47354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-	description: 'The new football ball of the world cup',
-});
-
 //Maquetacion de productos en la lista del contenedor principal de la pagina
 function renderProducts(productList) {
 	for (let product of productList) {
@@ -138,6 +143,14 @@ function renderProducts(productList) {
 		const productInfoFigureImg = document.createElement('img');
 		productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
 
+		
+		function shoppingListItems() {
+			shoppingList.push(product);
+			console.log(shoppingList);
+		}
+
+		productInfoFigure.addEventListener('click', shoppingListItems);
+
 		//Union de los elementos
 		productInfoFigure.append(productInfoFigureImg);
 		productInfoDiv.append(productInfoPrice, productInfoName);
@@ -145,6 +158,69 @@ function renderProducts(productList) {
 		productCard.append(img, productInfo);
 		cardsContainer.append(productCard);
 	}
+}
+
+//Maquetacion menu carrito de compras
+function renderCartMenu() {
+	
+		//Elemento Title container
+		const titleContainer = document.createElement('div');
+		titleContainer.classList.add('title-container');
+		const returnArrowImg = document.createElement('img');
+		returnArrowImg.setAttribute('src', './icons/flechita.svg');
+		returnArrowImg.setAttribute('alt', 'arow');
+		returnArrowImg.addEventListener('click', toggleCartMenu);
+		const title = document.createElement('p');
+		title.classList.add('title');
+		title.innerText = "My order";
+
+		titleContainer.append(returnArrowImg, title);
+
+		//Elemento my order content
+		const myOrderContent = document.createElement('div');
+		myOrderContent.classList.add('my-order-content');
+		const shoppingCart = document.createElement('div');
+		shoppingCart.classList.add('shopping-cart');
+		const productFigure = document.createElement('figure');
+		const productImg = document.createElement('img');
+		productImg.classList.add('product__img');
+		const productName = document.createElement('p');
+		productName.classList.add('product__name');
+		const productPrice = document.createElement('p');
+		productPrice.classList.add('product__price');
+		const deleteProduct = document.createElement('img');
+		deleteProduct.setAttribute('src','./icons/icon_close.png');
+		deleteProduct.setAttribute('alt','close');
+
+
+		
+
+
+
+		productFigure.append(productImg);
+		shoppingCart.append(productFigure,productName,productPrice,deleteProduct);
+
+			//Elemento order
+		const order = document.createElement('div');
+		order.classList.add('order');
+		const nameContainer = document.createElement('p');
+		const nameSpan = document.createElement('span');
+		nameSpan.innerText = "Total";
+		const totalPrice = document.createElement('p');
+		totalPrice.classList.add('total__price');
+
+		nameContainer.append(nameSpan)
+		order.append(nameContainer,totalPrice)
+			//Elemento button
+		const primaryButton = document.createElement('button');
+		primaryButton.classList.add('primary-button');
+		primaryButton.innerText = "Checkout";
+
+		myOrderContent.append(shoppingCart, order, primaryButton);
+
+		cartIconMenu.append(titleContainer, myOrderContent);
+	
+	
 }
 
 //Maquetacion del banner que se despliega con la informacion del producto al cual se da click
@@ -187,5 +263,8 @@ function renderSpecification() {
 	productDetailContainer.append(iconClose, productImg, productInfo);
 }
 
+
+
 renderProducts(productList);
+renderCartMenu();
 renderSpecification();
